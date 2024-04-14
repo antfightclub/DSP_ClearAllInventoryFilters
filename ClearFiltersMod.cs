cf6a48dc9d525e2adacb7e62323c019ee73dbe49
monapink <91607167+antfightclub@users.noreply.github.com>
2024-04-14 11:02:07 +0200

    Added Util.cs from Hetima and started CreateUI()

--- ClearFiltersMod.cs
+++ ClearFiltersMod.cs
@@ -1,14 +1,40 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 namespace ClearAllInventoryFilters
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class ClearFiltersMod : BaseUnityPlugin
     {
-        private void Awake()
+        void Awake()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+            harmony.PatchAll(typeof(Patch));
+
         }
+
+        public static void CreateUI()
+        {
+            // Grab instance of player inventory
+            UIInventoryWindow inventory = UIRoot.instance.uiGame.inventoryWindow;
+            GameObject inv = inventory.gameObject;
+            RectTransform windowTrans = inv.gameObject.GetComponent<RectTransform>(); 
+
+            UIDESwarmPanel swarmPanel = UIRoot.instance.uiGame.dysonEditor.controlPanel.hierarchy.swarmPanel;
+            UIButton src = swarmPanel.orbitButtons[0];
+            UIButton btn = GameObject.Instantiate<UIButton>(src);
+            RectTransform btnRect = Util.NormalizeRectWithTopLeft(btn, 50f, 50f, windowTrans);
+
+        }
+
+        static class Patch
+        {
+
+        }
+        
     }
 }
