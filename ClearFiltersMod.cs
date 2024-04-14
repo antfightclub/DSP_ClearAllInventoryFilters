0625cd49148c3afa62b1c05e64be82e649c56035
monapink <91607167+antfightclub@users.noreply.github.com>
2024-04-14 10:35:01 +0200

    rename main plugin file

--- ClearFiltersMod.cs
+++ ClearFiltersMod.cs
@@ -3,7 +3,7 @@
 namespace ClearAllInventoryFilters
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    public class ClearFiltersMod : BaseUnityPlugin
     {
         private void Awake()
         {
